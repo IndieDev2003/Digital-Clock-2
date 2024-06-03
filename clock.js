ac9f1fc27cb5eff6f0dc84e109ab1d1cc0f8b0a3
Gagan Suman <ilovescience584@gmail.com>
2024-06-03 11:19:09 +0530

    Build

---
+++ clock.js
@@ -0,0 +1,31 @@
+const events=["Its Anniversay My ","Happy Birthday My Cat "]
+let hrs = document.getElementById("hrs");
+let min = document.getElementById("min");
+let sec = document.getElementById("sec");
+
+let day=document.getElementById("date")
+let month=document.getElementById("month")
+let year=document.getElementById("year")
+
+const eventToday=document.getElementById("event-today")
+
+const monthName=["January","February","March","April","May","June","July","August","September","October","November","December"]
+
+
+setInterval(function(){
+    let time = new Date();
+    hrs.innerHTML = (time.getHours() < 10 ? '0':'') + time.getHours();
+    min.innerHTML = (time.getMinutes() < 10 ? '0':'') + time.getMinutes();
+    sec.innerHTML = (time.getSeconds() < 10 ? '0':'') + time.getSeconds();
+
+    day.innerHTML=(time.getDate()<10?'0':'')+time.getDate();
+    month.innerHTML=monthName[time.getMonth()];
+    year.innerHTML=time.getFullYear();
+    // console.log(time.toJSON())
+
+    if(time.getDate()===3){
+        eventToday.innerHTML="Its Anniversary My "
+    }else if(time.getDate()===18 && time.getMonth()===8){
+        eventToday.innerHTML=events[1]
+    }
+},1000);
